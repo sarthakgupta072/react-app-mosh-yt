0fa9dedb69a01e8ed7b81052ec3ccfe56bd1de93
Sarthak Gupta <sarthakgupta072@gmail.com>
2023-07-17 19:55:44 +0530

    feat: useState usage

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,14 +1,25 @@
+import { useState } from "react"
+
 function ListGroup() {
   let items = ["New York", "San Francisco", "Delhi", "Mumbai"]
-  items = []
+
+  const [selectedIndex, setSelectedIndex] = useState(-1)
 
   return (
     <>
       <h1>List</h1>
       {items.length === 0 && <p>No Items Found</p>}
       <ul className='list-group'>
-        {items.map((item, id) => (
-          <li key={id} className='list-group-item'>
+        {items.map((item, index) => (
+          <li
+            key={index}
+            onClick={() => setSelectedIndex(index)}
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+          >
             {item}
           </li>
         ))}
